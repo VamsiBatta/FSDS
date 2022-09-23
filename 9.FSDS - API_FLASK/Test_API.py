8aeb414ead77c7e4fd690456e6b2b2ba9f8617bf
VamsiBatta <bvamsi.1036@gmail.com>
2022-09-23 17:19:37 +0530

    Updated API_FLASK with Test_API

--- 9.FSDS - API_FLASK/Test_API.py
+++ 9.FSDS - API_FLASK/Test_API.py
@@ -1,12 +1,46 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
 
 
+@app.route('/xyz', methods=["GET", "POST"])
+def test():
+    if request.method == "POST":
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a + b
+        return jsonify(result)
 
 
-def test(a, b):
-    print(a + b)
-    return a + b
+@app.route('/xyz1', methods=["GET", "POST"])
+def test1():
+    if request.method == "POST":
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a - b
+        return jsonify(result)
 
 
-test(4, 4)
+@app.route('/xyz2', methods=["GET", "POST"])
+def test2():
+    if request.method == "POST":
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a * b
+        return jsonify(result)
 
+
+@app.route('/xyz3', methods=["GET", "POST"])
+def test3():
+    if request.method == "POST":
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a // b
+        return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run()
+
+# 1. Write a Function to Fetch data from sql table via API
+# 2. write a Function to fetch data from mongodb table
