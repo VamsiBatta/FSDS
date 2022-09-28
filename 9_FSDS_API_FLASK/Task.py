555b9bd94b0b98e5ce8547c651987aa807636a7e
VamsiBatta <bvamsi.1036@gmail.com>
2022-09-28 15:08:45 +0530

    Completed Task-1

--- 9_FSDS_API_FLASK/Task.py
+++ 9_FSDS_API_FLASK/Task.py
@@ -1,8 +1,20 @@
 import mysql.connector as conn
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
 
 mydb = conn.connect(host="localhost", user="root", passwd="12345")
 c = mydb.cursor()
-c.execute("show databases")
-x = c.fetchall()
-[print(i) for i in x]
+c.execute("use exampledb")
 
+
+@app.route("/abc", methods=['GET', 'POST'])
+def task1():
+    if request.method == "GET":
+        c.execute("select * from glass_data")
+        x = c.fetchall()
+        return jsonify(str(x))
+
+
+if __name__ == "__main__":
+    app.run()
--- 9_FSDS_API_FLASK/Test_API.py
+++ 9_FSDS_API_FLASK/Test_API.py
@@ -12,5 +12,38 @@ def test():
         return jsonify(str(result))
 
 
+@app.route("/xyz1", methods=['GET', 'POST'])
+def test1():
+    if request.method == "POST":
+        a = request.json["num1"]
+        b = request.json["num2"]
+        result = a - b
+        return jsonify(str(result))
+
+
+@app.route("/xyz2", methods=['GET', 'POST'])
+def test2():
+    if request.method == "POST":
+        a = request.json["num1"]
+        b = request.json["num2"]
+        result = a * b
+        return jsonify(str(result))
+
+
+@app.route("/xyz3", methods=['GET', 'POST'])
+def test3():
+    if request.method == "POST":
+        a = request.json["num1"]
+        b = request.json["num2"]
+        result = a / b
+        return jsonify(str(result))
+
+
 if __name__ == "__main__":
     app.run()
+
+
+# TASK
+
+# 1. Write a Function to fetch data from sql table via api
+# 2. Write a function to fetch data from a mongodb file
